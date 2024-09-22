26ca60ffd5ba10bcab959442e79f25e979bcacde
mdemiguels <miguelschz1997@gmail.com>
2024-09-22 12:52:42 +0200

    finish 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+    word = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    
+    let wordReversed = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        wordReversed += word[i];
+    }
+
+    return wordReversed === word;
 
 };
 
