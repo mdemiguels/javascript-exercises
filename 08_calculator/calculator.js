90862edd7372ccb32e46e8eb331d9dfc81fd0dbd
mdemiguels <miguelschz1997@gmail.com>
2024-09-22 12:35:43 +0200

    finish 08-calculator section

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+
+	let sum = 0;
+  array.forEach(x => {
+    sum += x;
+  });
+
+  return sum;
+
 };
 
-const multiply = function() {
+const multiply = function(array) {
+
+  let prod = 1;
+
+  array.forEach(x => {
+    prod *= x;
+  });
+
+  return prod;
 
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
+const factorial = function(a) {
+
+  let prod = 1;
+  for (let i = 1; i <= a; i++) {
+    prod *= i;
+  }
+
+  return prod;
 	
 };
 
